72070d593c60786c68e1a158b1e0a77d22b9c995
billy.pentney@outlook.com <billy.pentney@outlook.com>
2024-07-31 15:03:35 +0100

    Rename iguanodontid to iguanodontian

--- app/src/main/java/com/bp/dinodata/presentation/StringConverters.kt
+++ app/src/main/java/com/bp/dinodata/presentation/StringConverters.kt
@@ -31,7 +31,7 @@ fun convertCreatureTypeToString(type: CreatureType): String? {
         CreatureType.MediumTheropod -> stringResource(R.string.type_medium_theropod)
         CreatureType.Plesiosaur -> stringResource(R.string.type_plesiosaur)
         CreatureType.Therizinosaurid -> stringResource(R.string.type_therizinosaurid)
-        CreatureType.Iguanodontid -> stringResource(R.string.type_iguanodontid)
+        CreatureType.Iguanodontid -> stringResource(R.string.type_iguanodontian)
         CreatureType.Crocodilian -> stringResource(R.string.type_crocodilian)
         CreatureType.Serpent -> stringResource(R.string.type_serpent)
     }
